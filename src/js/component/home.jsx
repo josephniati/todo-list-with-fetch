f5173764dddbf4f6b2af8b82055e2bfe0a60b1b0
Joseph Niati <emcmonkey12@gmail.com>
2022-05-19 02:48:36 +0000

    adding get/put functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
 import React from "react";
+import { useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = React.useState("");
+	const [taskList, setTaskList] = React.useState([]);
+
+	const saveTodo = async function (newTask) {
+		var options = {
+			method: "PUT",
+			body: JSON.stringify(newTask),
+			headers: { "content-type": "application/json" },
+		};
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jniati",
+			options
+		);
+		setTaskList([...taskList, { label: task, done: false }]);
+	};
+	const retrieveTodo = async function () {
+		var options = {
+			method: "GET",
+		};
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/jniati",
+			options
+		);
+		setTaskList(await response.json());
+	};
+	useEffect(() => {
+		retrieveTodo();
+	}, []);
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				value={task}
+				onChange={(e) => setTask(e.target.value)}></input>
+			<button
+				onClick={() =>
+					saveTodo([...taskList, { label: task, done: false }])
+				}>
+				Submit
+			</button>
+			<div>
+				<div>
+					<ul>
+						{taskList.map((item, i) => {
+							return (
+								<li
+									key={i}
+									onClick={() =>
+										setTaskList(
+											taskList.filter(
+												(dTask, x) => x != i
+											)
+										)
+									}>
+									{item.label} <button>Trash</button>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>{" "}
 		</div>
 	);
 };
